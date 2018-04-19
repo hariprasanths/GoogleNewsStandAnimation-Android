1e2bcd74c77f39b86b32afc17e623c7b5e4d56fd
Hariprasanth <shhariprasanth@gmail.com>
2018-04-19 22:01:18 +0530

    Initial commit

---
+++ app/src/main/java/hari/allagisample/MainActivity.java
@@ -0,0 +1,15 @@
+package hari.allagisample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hari.allagi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
